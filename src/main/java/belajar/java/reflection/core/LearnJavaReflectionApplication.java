273ab393f8c072cd45a35d53859fb6d1889d24e9
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-15 05:28:13 +0700

    first commit

---
+++ src/main/java/belajar/java/reflection/core/LearnJavaReflectionApplication.java
@@ -0,0 +1,13 @@
+package belajar.java.reflection.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaReflectionApplication.class, args);
+	}
+
+}
